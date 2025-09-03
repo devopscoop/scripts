ebd377ede349135ab88f3fce228bcc0ecd4d18d8
Evans Tucker <gmail@evanstucker.com>
2025-09-03 13:12:28 -0700

    Minor improvements

--- upgrade_opentofu_modules.sh
+++ upgrade_opentofu_modules.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Delete lock file, because OpenTofu currently has readonly access to it, so it won't update anything if it exists.
-rm .terraform.lock.hcl
+rm -rf .terraform.lock.hcl .terraform
 
 # Comment out all module versions so `tofu init -upgrade` can pull the latest versions.
 grep -rI --exclude-dir .git --exclude-dir .terraform -E '^[ ]+version[ ]+=' -l | xargs sed -Ei 's/^([ ]+)version /#\1version /'
@@ -9,7 +9,8 @@ grep -rI --exclude-dir .git --exclude-dir .terraform -E '^[ ]+version[ ]+=' -l |
 # Upgrade everything!
 tofu init -upgrade
 
-# Show new versions?
+echo -e "\nThese are the new module versions you should use in your *.tf files:\n"
 jq -r '.Modules[] | "\(.Source) \(.Version)"' .terraform/modules/modules.json | sort -u
 
-echo "Now open a new terminal and go manually update the versions and uncomment them..."
+echo -e "\nThere are the *.tf files that need to be manually edited. Find the version above, add it to the file, then uncomment the version line.\n"
+git diff HEAD ./*.tf
